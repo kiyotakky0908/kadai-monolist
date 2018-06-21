0fbab44ce59a8391cd0b2223d1063769996d2dca
saburo.kirameki <ec2-user@ip-172-31-5-128.us-east-2.compute.internal>
2018-06-21 03:38:11 +0000

    init

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+
+use App\Item;
+
+class WelcomeController extends Controller
+{
+    public function index()
+    {
+     $items = Item::orderBy('updated_at', 'desc')->paginate(20);
+        return view('welcome', [
+            'items' => $items,
+        ]);
+       
+}
+}
